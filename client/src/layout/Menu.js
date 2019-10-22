2a35ad6c3565ffbc754db3ecc69adb3d27df7f12
Kim Hyunwoo <snltpdua@gmail.com>
2019-10-22 21:16:34 +0900

    공시 게시판, 네비바 보수
    부트스트랩으로 회귀

--- client/src/layout/Menu.js
+++ client/src/layout/Menu.js
@@ -6,51 +6,49 @@ import { logout } from "../actions/auth";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-import { Button, Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
-
 const Menu = ({ auth: { isAuthenticated, loading }, logout }) => {
   // 인증된 사용자 접속시 나타나는 메뉴
   const authLinks = (
-    <Nav>
-      <NavItem>
-        <NavLink href="/uploadholders/">명부 업로드</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/vote/">투표</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/bulletin/">보드</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/myaccount/">내 정보</NavLink>
-      </NavItem>
-      <NavItem>
-        <Button color="link" onClick={logout}>
+    <ul className="nav">
+      <li className="nav-item">
+        <a className="nav-link text-light" href="/uploadholders/">명부 업로드</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link text-light" href="/vote/">투표</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link text-light" href="/bulletin/">보드</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link text-light" href="/myaccount/">내 정보</a>
+      </li>
+      <li className="nav-item">
+        <button type="button" className="btn btn-outline-primary" color="link" onClick={logout}>
           로그아웃
-        </Button>
-      </NavItem>
-    </Nav>
+        </button>
+      </li>
+    </ul>
   );
 
   // 인증되지 않은 사용자 접속시 나타나는 메뉴
   const guestLinks = (
-    <Nav>
-      <NavItem>
-        <NavLink href="/login/">로그인</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/register">회원가입</NavLink>
-      </NavItem>
-    </Nav>
+    <ul className="nav">
+      <li>
+        <a className="nav-link" href="/login/">로그인</a>
+      </li>
+      <li>
+        <a className="nav-link" href="/register">회원가입</a>
+      </li>
+    </ul>
   );
 
   return (
-    <Navbar color="light" light expand="md">
-      <NavbarBrand href="/">JouJou</NavbarBrand>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <a class="navbar-brand" href="/">JouJou</a>
       {!loading && (
         <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
       )}
-    </Navbar>
+    </nav>
   );
 };
 
