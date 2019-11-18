bc689f3b1f40d6e682fd587ff9fa5189ecdd3491
yeeun <aweed0855@gmail.com>
2019-11-18 21:33:34 +0900

    오류 수정

--- client/src/layout/Vote.js
+++ client/src/layout/Vote.js
@@ -10,7 +10,7 @@ import { get } from "http";
 const Vote = ({ auth: { user, currentAccount, loading } }) => {
   useEffect(() => {
     getVote().then(data => {
-      setValues({data:data.data.contents})
+      setValues({ data: data.data.contents })
     })
   }, [getVote]);
 
@@ -20,119 +20,76 @@ const Vote = ({ auth: { user, currentAccount, loading } }) => {
   });
   console.log(user);
   console.log(values.data)
-  let arr =[1,2,3,4,5]
 
   return (
-    values.data === null ? ( "loading ..." ) : (
-    <>
-      <h1>투표 기능</h1>
-      <div>
+    values.data === null || loading === null ? ("loading ...") : (
+      <>
+        <h1>투표 기능</h1>
+        <div>
+          <table className="table">
+            <thead>
+              <tr>
+                <td>전자투표권자명</td>
+                <td>주주 구분</td>
+                <td>지갑 주소</td>
+                <td>보유 주식 수</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{user.username}</td>
+                <td>일반 투표권자</td>
+                <td>{currentAccount}</td>
+                <td>524주</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <br />
+
         <table className="table">
+          <colgroup>
+            <col width="15%" />
+            <col />
+            <col width="30%"/>
+          </colgroup>
           <thead>
             <tr>
-              <td>전자투표권자명</td>
-              <td>주주 구분</td>
-              <td>지갑 주소</td>
-              <td>보유 주식 수</td>
+              <th>의안 번호</th>
+              <th>의안 내용</th>
+              <th>의견</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>{user.username}</td>
-              <td>일반 투표권자</td>
-              <td>{currentAccount}</td>
-              <td>524주</td>
-            </tr>
+            {values.data.map((data, index) => (
+              <tr key={index}>
+                <td align="center">{index + 1}</td>
+                <td>{data}</td>
+                <td>
+                  <select className="browser-default custom-select">
+                    <option defaultValue>찬성/반대</option>
+                    <option value="agree">찬성</option>
+                    <option value="disagree">반대</option>
+                  </select>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
-      </div>
-      <br />
 
-      <table className="table">
-        <thead>
-          <tr>
-            <th>의안 번호</th>
-            <th>의안 내용</th>
-            <th>의견</th>
-          </tr>
-        </thead>
-        <tbody>
-          {values.data.map((data, index) => (
-            <tr key={index}>
-              <td align="center">{index + 1}</td>
-              <td>{data}</td>
-              <td>
-                <select className="browser-default custom-select">
-                  <option defaultValue>찬성/반대</option>
-                  <option value="agree">찬성</option>
-                  <option value="disagree">반대</option>
-                </select>
-              </td>
-            </tr>
-          ))}
-
-          {/* <tr>
-            <td>1</td>
-            <td>사장 교체의 건</td>
-            <td>
-              <select className="browser-default custom-select">
-                <option defaultValue>찬성/반대</option>
-                <option value="agree">찬성</option>
-                <option value="disagree">반대</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>1 - 2</td>
-            <td>대표이사 선임의 건</td>
-            <td>
-              <select className="browser-default custom-select">
-                <option defaultValue>찬성/반대</option>
-                <option value="agree">찬성</option>
-                <option value="disagree">반대</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>1 - 3</td>
-            <td>임원급 직원 승진의 건</td>
-            <td>
-              <select className="browser-default custom-select">
-                <option defaultValue>찬성/반대</option>
-                <option value="agree">찬성</option>
-                <option value="disagree">반대</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>합작투자 유치에 관한 건</td>
-            <td>
-              <select className="browser-default custom-select">
-                <option defaultValue>찬성/반대</option>
-                <option value="agree">찬성</option>
-                <option value="disagree">반대</option>
-              </select>
-            </td>
-          </tr> */}
-        </tbody>
-      </table>
-
-      <div className="button">
-        <p className="text-right">
-          <button type="button" className="btn btn-primary">
-            초기화
+        <div className="button">
+          <p className="text-right">
+            <button type="button" className="btn btn-primary">
+              초기화
           </button>{" "}
-          &nbsp; &nbsp;
+            &nbsp; &nbsp;
           <button type="button" className="btn btn-primary">
-            제출
+              제출
           </button>
-        </p>
-      </div>
-    </>
-    )
-  )
-};
+          </p>
+        </div>
+      </>
+    ))};
 
 Vote.propTypes = {
   auth: PropTypes.object.isRequired
