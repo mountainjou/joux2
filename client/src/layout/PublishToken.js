1e3e0c38b2f7020a2151484dc65114e1c8061abf
kimkihogn <ganzik83@gmail.com>
2019-11-18 23:14:41 +0900

    Auto stash before merge of "Kihong" and "origin/Kihong"

--- client/src/layout/PublishToken.js
+++ client/src/layout/PublishToken.js
@@ -1,33 +1,33 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import Web3 from 'web3';
-import Axios from 'axios';
-import Spinner from '../components/Spinner';
-import { ethers } from 'ethers';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Web3 from "web3";
+import Axios from "axios";
+import Spinner from "../components/Spinner";
+import { ethers } from "ethers";
 
-import Alert from '../Alert';
-import { setAlert } from '../actions/alert';
+import Alert from "../Alert";
+import { setAlert } from "../actions/alert";
 
-import { abi, bytecode } from '../contracts/Token.json'; // 컴파일된 Token 컨트랙트에서 abi값과 bytecode값을 가져온다.
+import { abi, bytecode } from "../contracts/Token.json"; // 컴파일된 Token 컨트랙트에서 abi값과 bytecode값을 가져온다.
 
 const PublishToken = ({
   setAlert,
   auth: { user, loading, currentAccount }
 }) => {
   // const web3 = new Web3(Web3.givenProvider);
-  const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8546');
+  const web3 = new Web3(Web3.givenProvider || "ws://localhost:8546");
 
   const [values, setValues] = React.useState({
-    totalStocks: '',
-    tokenName: '',
-    tokenSymbol: '',
-    holders: ''
+    totalStocks: "",
+    tokenName: "",
+    tokenSymbol: "",
+    holders: ""
   });
 
   // 주주명부 가져오기
   useEffect(() => {
-    Axios.get('/api/upload')
+    Axios.get("/api/upload")
       .then(result => {
         console.log(result);
         setValues({
@@ -93,16 +93,16 @@ const PublishToken = ({
           console.log(transactionHash);
         }
       )
-      .on('error', err => {})
-      .on('transactionHash', transactionHash => {})
-      .on('receipt', receipt => {
+      .on("error", err => {})
+      .on("transactionHash", transactionHash => {})
+      .on("receipt", receipt => {
         console.log(receipt.contractAddress); // contains the new contract address
       })
-      .on('confirmation', (confirmationNumber, receipt) => {})
+      .on("confirmation", (confirmationNumber, receipt) => {})
       .then(newContractInstance => {
         console.log(newContractInstance);
         console.log(newContractInstance.options.address); // instance with the new contract address
-        const url = '/api/users/registercontractaddress';
+        const url = "/api/users/registercontractaddress";
         const data = {
           email: user.email,
           ca: newContractInstance.options.address,
@@ -110,7 +110,7 @@ const PublishToken = ({
         };
         const config = {
           headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json"
           }
         };
 
@@ -128,7 +128,7 @@ const PublishToken = ({
   return loading === null ? (
     <Spinner />
   ) : (
-    <div className='container'>
+    <div className="container">
       <div>토큰 발행</div>
       <br />
       {/* <div>
@@ -145,58 +145,58 @@ const PublishToken = ({
       </div> */}
 
       <form onSubmit={e => publishTokenFromContract(e)}>
-        <div className='form-group'>
-          <label htmlFor='totalStocks'>주주명부에 등록된 총 주식 발행량</label>
+        <div className="form-group">
+          <label htmlFor="totalStocks">주주명부에 등록된 총 주식 발행량</label>
           <div>
             <input
-              type='text'
-              className='form-control'
+              type="text"
+              className="form-control"
               readOnly
-              id='totalStocks'
+              id="totalStocks"
               value={totalStocks}
             />
           </div>
         </div>
-        <div className='form-group'>
-          <label htmlFor='corporation'>회사명</label>
+        <div className="form-group">
+          <label htmlFor="corporation">회사명</label>
           <div>
             <input
-              type='text'
-              className='form-control'
-              id='corporation'
+              type="text"
+              className="form-control"
+              id="corporation"
               readOnly
-              value={user.corporation ? user.corporation.name : ''}
+              value={user.corporation ? user.corporation.name : ""}
             />
           </div>
         </div>
-        <div className='form-group'>
-          <label htmlFor='tokenName'>토큰 이름</label>
+        <div className="form-group">
+          <label htmlFor="tokenName">토큰 이름</label>
           <div>
             <input
-              type='text'
-              className='form-control'
-              id='tokenName'
-              placeholder='발행 할 토큰 이름을 입력하세요'
-              onChange={handleChange('tokenName')}
+              type="text"
+              className="form-control"
+              id="tokenName"
+              placeholder="발행 할 토큰 이름을 입력하세요"
+              onChange={handleChange("tokenName")}
             />
           </div>
         </div>
-        <div className='form-group'>
-          <label htmlFor='tokenSymbol'>토큰 심볼</label>
+        <div className="form-group">
+          <label htmlFor="tokenSymbol">토큰 심볼</label>
           <div>
             <input
-              type='text'
-              className='form-control'
-              id='tokenSymbol'
-              placeholder='토큰 심볼을 입력하세요'
-              onChange={handleChange('tokenSymbol')}
+              type="text"
+              className="form-control"
+              id="tokenSymbol"
+              placeholder="토큰 심볼을 입력하세요"
+              onChange={handleChange("tokenSymbol")}
             />
           </div>
         </div>
         <br />
         <button
-          type='button'
-          className='btn btn-primary'
+          type="button"
+          className="btn btn-primary"
           onClick={publishTokenFromContract}
         >
           발행하기
